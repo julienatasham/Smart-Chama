a004d87643d26de635c700913658c02d63500741
julienatasham <julienatasha.m@gmail.com>
2025-06-05 15:09:55 +0300

    Update summary.py

--- chama/summary.py
+++ chama/summary.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
 def show_summary(chama_members):
+    """
+    Shows member list and total contributions.
+    """
     if not chama_members:
         st.info("No members added yet.")
         return
@@ -8,5 +11,4 @@ def show_summary(chama_members):
     for member in chama_members:
         st.write(f"- {member['name']}: KES {member['contribution']}")
         total += member['contribution']
-    st.write(f"💰 Total Contributions: KES {total}")
-
+    st.markdown(f"### 💰 Total Contributions: KES {total}")
