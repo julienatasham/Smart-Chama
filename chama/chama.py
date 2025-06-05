3b527668ed28d97cfb8264b0297985b2f404fb0a
julienatasham <julienatasha.m@gmail.com>
2025-06-05 16:18:11 +0300

    Update chama.py

--- chama/chama.py
+++ chama/chama.py
@@ -1,5 +1,30 @@
-def create_chama(name):
-    """
-    Accepts and returns the Chama group name.
-    """
-    return name
+import json
+import os
+from chama.data_handler import load_data, save_data
+
+CHAMA_FILE = 'chama_data.json'
+
+def load_chamas():
+    if not os.path.exists(CHAMA_FILE):
+        return {}
+    return load_data(CHAMA_FILE)
+
+def save_chamas(data):
+    save_data(CHAMA_FILE, data)
+
+def load_chamas_for_user(email):
+    data = load_chamas()
+    return data.get(email, [])
+
+def create_chama_for_user(email, chama_name):
+    data = load_chamas()
+    user_chamas = data.get(email, [])
+    new_chama = {
+        "id": len(user_chamas) + 1,
+        "name": chama_name,
+        "members": [],
+        "transactions": []
+    }
+    user_chamas.append(new_chama)
+    data[email] = user_chamas
+    save_chamas(data)
