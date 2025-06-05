7497aaa955eabecd3a5ff7b58a44e179c8a16150
julienatasham <julienatasha.m@gmail.com>
2025-06-05 15:27:01 +0300

    Update members.py

--- chama/members.py
+++ chama/members.py
@@ -1,10 +1,7 @@
-def add_member(name, amount, chama_members):
-    """
-    Adds a member dictionary to chama_members list.
-    """
-    if not name or amount <= 0:
-        return False
-    chama_members.append({'name': name, 'contribution': amount})
-    return True
+# chama/members.py
 
-
+def add_member(chama_members, name, amount):
+    chama_members.append({
+        'name': name,
+        'contribution': amount
+    })
